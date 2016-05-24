2e8351cb2c8db8044aea9999ece934d9b4d311db
zzyzy <leezhenzhi@gmail.com>
2016-05-24 15:37:22 +1000

    Added pages

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'meteor/iron:router';
 
 import '../../ui/layouts/app-body.js';
-import '../../ui/components/projects-list.js';
+import '../../ui/pages/dashboard.js';
 
 Router.route('/', function() {
   this.layout('appBody');
-  this.render('projectsList');
+  this.render('dashboard');
 });
