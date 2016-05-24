7ac40090747ab33cddb34ceb5c43f3bc473afc99
zzyzy <leezhenzhi@gmail.com>
2016-05-24 16:29:41 +1000

    Individual issue items can now be viewed

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,26 +1,37 @@
 import { Router } from 'meteor/iron:router';
 
 import { Projects } from '../../api/projects.js';
+import { Issues } from '../../api/issues.js';
 
 import '../../ui/layouts/app-body.js';
 import '../../ui/pages/dashboard.js';
 import '../../ui/pages/product-backlog.js'
+import '../../ui/components/issue-item-view.js';
 
-Router.route('/', function() {
+Router.route('/', function () {
   this.layout('appBody');
   this.render('dashboard');
 });
 
-Router.route('/projects/', function() {
+Router.route('/projects/', function () {
   this.layout('appBody');
   this.render('dashboard');
 });
 
-Router.route('/projects/:_id', function() {
+Router.route('/projects/:project_id', function () {
   this.layout('appBody');
   this.render('productBacklog', {
-    data: function() {
-      return Projects.findOne({_id: this.params._id});
+    data: function () {
+      return Projects.findOne({_id: this.params.project_id});
     }
   });
+});
+
+Router.route('/projects/:project_id/issues/:issue_id', {
+  name: 'issue',
+  template: 'issueItemView',
+  layoutTemplate: 'appBody',
+  data: function() {
+    return Issues.findOne({_id: this.params.issue_id});
+  }
 });
--- imports/ui/items/issue-item.js
+++ imports/ui/items/issue-item.js
@@ -1,1 +1,9 @@
+import { Template } from 'meteor/templating';
+
 import './issue-item.html';
+
+Template.issueItem.helpers({
+  projectId() {
+    return this.projectId;
+  }
+});
