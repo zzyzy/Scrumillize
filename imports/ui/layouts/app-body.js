c04e65ea2e4d213af73c8d43240218b37ea0353a
zzyzy <leezhenzhi@gmail.com>
2016-05-26 02:18:33 +1000

    Fixed appBody sidebar

--- imports/ui/layouts/app-body.js
+++ imports/ui/layouts/app-body.js
@@ -1,12 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Router } from 'meteor/iron:router';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import { Projects } from '../../api/projects.js';
 
 import './app-body.html';
 
 Template.appBody.onCreated(function () {
-  this.projectId = Router.current().params.project_id;
-  console.log(Meteor.user());
+  this.projectId = new ReactiveVar();
+  this.projectId.set(Router.current().params.project_id);
+  this.autorun(function () {
+    const projectId = Router.current().params.project_id;
+    Template.instance().projectId.set(projectId);
+  });
   if (!Meteor.user()) {
     // Router.go('signin');
   }
@@ -14,10 +21,15 @@ Template.appBody.onCreated(function () {
 
 Template.appBody.helpers({
   projectId () {
-    return Template.instance().projectId;
+    return Template.instance().projectId.get();
   },
   isCurrentRoute (name) {
     return name === Router.current().route.getName();
+  },
+  projectName () {
+    const currentProject = Projects.findOne({_id: Template.instance().projectId.get()});
+    if (currentProject === null || currentProject === undefined) return '';
+    else return currentProject.projectName;
   }
 });
 
