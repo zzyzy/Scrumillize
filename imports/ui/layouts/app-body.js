35d058d522ea90d7e8ab692d3fa7d773c65a967d
zzyzy <leezhenzhi@gmail.com>
2016-05-26 01:50:53 +1000

    Slight ui changes

--- imports/ui/layouts/app-body.js
+++ imports/ui/layouts/app-body.js
@@ -1,12 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { Router } from 'meteor/iron:router';
 
 import './app-body.html';
 
 Template.appBody.onCreated(function () {
+  this.projectId = Router.current().params.project_id;
   console.log(Meteor.user());
   if (!Meteor.user()) {
     // Router.go('signin');
+  }
+});
+
+Template.appBody.helpers({
+  projectId () {
+    return Template.instance().projectId;
+  },
+  isCurrentRoute (name) {
+    return name === Router.current().route.getName();
   }
 });
 
