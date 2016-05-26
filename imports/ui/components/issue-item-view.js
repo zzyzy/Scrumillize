ecd75aa749394b52ff52bb0543b2a433da483b0f
zzyzy <leezhenzhi@gmail.com>
2016-05-26 23:45:31 +1000

    Styled issue item view

--- imports/ui/components/issue-item-view.js
+++ imports/ui/components/issue-item-view.js
@@ -2,9 +2,59 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
+import { Sprints } from '../../api/sprints.js';
 import { Releases } from '../../api/releases.js';
 
 import './issue-item-view.html';
+
+Template.issueItemView.helpers({
+  releaseName (releaseId) {
+    const release = Releases.findOne({_id: releaseId});
+    if (release === null || release === undefined) return 'None';
+    else return release.releaseName;
+  },
+  getType () {
+    const type = Template.instance().data.type;
+    return type.charAt(0).toUpperCase() + type.substring(1);
+  },
+  getStatus () {
+    const status = Template.instance().data.status;
+    return status.charAt(0).toUpperCase() + status.substring(1);
+  },
+  getPriority () {
+    const priority = Template.instance().data.priority;
+    return priority.charAt(0).toUpperCase() + priority.substring(1);
+  },
+  getResolution() {
+    const resolution = Template.instance().data.resolution;
+    if (resolution === false) return 'Unresolved';
+    else return 'Done';
+  },
+  sprintName (sprintId) {
+    const sprint = Sprints.findOne({_id: sprintId});
+    if (sprint === null || sprint === undefined) return 'None';
+    else return sprint.sprintName;
+  },
+  getAssignee () {
+    const assignee = Template.instance().data.assignee;
+    if (assignee === null || assignee === undefined) return 'Unassigned';
+    else {
+      const user = Meteor.users.findOne({_id: assignee});
+      return user.emails[0].address;
+    }
+  },
+  getReporter () {
+    const reporter = Template.instance().data.assignee;
+    if (reporter === null || reporter === undefined) return 'None';
+    else {
+      const user = Meteor.users.findOne({_id: reporter});
+      return user.emails[0].address;
+    }
+  },
+  formatDate (date) {
+    return moment(date).format('DD/MMM/YY');
+  }
+});
 
 Template.addToReleaseModal.onCreated(function () {
   this.state = new ReactiveDict();
@@ -18,9 +68,6 @@ Template.addToReleaseModal.helpers({
   isChosenRelease (releaseId) {
     return Template.instance().state.get('chosenRelease') === releaseId;
   },
-  releaseName (releaseId) {
-    return Releases.findOne({ _id: releaseId }).releaseName;
-  }
 });
 
 Template.addToReleaseModal.events({
--- imports/ui/pages/browse-issue.js
+++ imports/ui/pages/browse-issue.js
@@ -1,5 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { Router } from 'meteor/iron:router';
 
 import '../components/issue-item-view.js';
 import './browse-issue.html';
+
+Template.browseIssue.onCreated(function () {
+  this.projectId = Router.current().params.project_id;
+});
+
+Template.browseIssue.helpers({
+  projectId () {
+    
+    return Template.instance().projectId;
+  }
+});
